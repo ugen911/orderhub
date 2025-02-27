e9f35ac5ad27e5332e3fd979228b7b94ab18a078
Evgeni Stroikin <evgenistroikin@gmail.com>
2025-02-27 22:45:43 +0700

    DB start

---
+++ db/config.py
@@ -0,0 +1,28 @@
+
+# Параметры подключения к локальной базе данных
+local_db_config = {
+    'user': 'postgres',
+    'password': '89232808797',
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'ugkorea'
+}
+
+
+# Данные для подключения к удаленной базе данных
+remote_db_config = {
+    'user': 'postgres',
+    'password': '785612',
+    'host': '26.218.196.12',
+    'port': '5432',
+    'database': 'stroikin'
+}
+
+# Данные для подключения к серверной базе данных
+server_db_config = {
+    'user': 'postgres',
+    'password': '785612',
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'stroikin'
+}
